bf297e4cbd3eddeab15c65fd77f5b5c358b96c17
Jansser Costa <Jansser Costa>
2016-05-28 11:43:23 -0300

    Testing...

---
+++ config/protractor.js
@@ -0,0 +1,11 @@
+exports.config = {
+	specs: ['../test/e2e/**/*.js'],
+	onPrepare: function () {
+		browser.driver.get('http://localhost:3000');
+		browser.driver.findElement(by.id('entrar')).click();
+
+		browser.driver.findElement(by.id('login_field')).sendKeys('jansser_costa@hotmail.com');
+		browser.driver.findElement(by.id('password')).sendKeys('jan9610');
+		browser.driver.findElement(by.name('commit')).click();
+	}
+};
--- test/spec/ContatoControllerSpec.js
+++ test/spec/ContatoControllerSpec.js
@@ -1,5 +1,34 @@
 describe('ContatoController', function () {
-	if('Deve criar um Contato vazio quando nenhum parâmetro de rota for passado', function () {
-		expect($scope.contato._id).toBeUndefined();
+	var $scope, $httpBackend;
+
+	beforeEach(function () {
+		module('contatooh');
+		inject(function ($injector) {
+			$scope = $injector.get('$rootScope').$new();
+			$httpBackend = $injector.get('$httpBackend');
+
+			$httpBackend.when('GET', '/contatos/1')
+				.respond({_id: '1'});
+			
+			$httpBackend.when('GET', '/contatos')
+				.respond([{}]);
+		});
 	});
+
+	it('Deve criar um Contato vazio quando nenhum parâmetro de rota for passado', inject(function ($controller) {
+		$controller('ContatosController', 
+			{'$scope': $scope});
+		
+		expect($scope.contato._id).toBeUndefined();
+	}));
+
+	it('Deve preencher o Contato vazio quando o parâmetro de rota for passado', inject(function ($controller) {
+		$controller('ContatosController', {
+				$routeParams: {id: 1},
+				'$scope': $scope 
+		});	
+
+		$httpBackend.flush();
+		expect($scope.contato._id).toBeDefined();
+	}));
 });
